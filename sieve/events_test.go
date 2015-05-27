c4a4a5145ddcee064e1da2b58b204f683a822229
Vail Gold <vail@bam-x.com>
2015-05-27 15:42:39 -0400

    Adding tests for sieve event summary logic.

--- sieve/events_test.go
+++ sieve/events_test.go
@@ -37,3 +37,97 @@ func (s *SieveEventsTestSuite) TestSieveUsesTimestampProvidedForTestEvent(c *che
 	
 	c.Assert(eventSummary.Timestamp, check.Equals, timestampString)
 }
+
+func (s *SieveEventsTestSuite) TestSieveCalculatesSecondToDateCorrectly(c *check.C) {
+	var url string
+	
+	timestampString := "2015-01-01T01:00:42-04:00"
+	url = "http://localhost:9966/events/test"
+    jsonData := []byte(stringutils.Concat(`{"timestamp":"`, timestampString, `"}`))
+	
+	httputils.PostDataToUrl(url, "application/json", jsonData)
+	httputils.PostDataToUrl(url, "application/json", jsonData)
+	httputils.PostDataToUrl(url, "application/json", jsonData)
+	
+	url = stringutils.Concat("http://localhost:9966/events/test?timestamp=", timestampString)
+	data, _ := httputils.GetDataFromUrl(url)
+	
+	var eventSummary sieve.EventSummary
+	json.Unmarshal(data, &eventSummary)
+	c.Assert(eventSummary.SecondToDate, check.Equals, 3)
+}
+
+func (s *SieveEventsTestSuite) TestSieveCalculatesMinuteToDateCorrectly(c *check.C) {
+	var url string
+	
+	url = "http://localhost:9966/events/test"
+	httputils.PostDataToUrl(url, "application/json", []byte(`{"timestamp":"2015-01-01T01:00:01-04:00"}`))
+	httputils.PostDataToUrl(url, "application/json", []byte(`{"timestamp":"2015-01-01T01:00:15-04:00"}`))
+	httputils.PostDataToUrl(url, "application/json", []byte(`{"timestamp":"2015-01-01T01:00:22-04:00"}`))
+	httputils.PostDataToUrl(url, "application/json", []byte(`{"timestamp":"2015-01-01T01:00:38-04:00"}`))
+	httputils.PostDataToUrl(url, "application/json", []byte(`{"timestamp":"2015-01-01T01:00:42-04:00"}`))
+	
+	url = "http://localhost:9966/events/test?timestamp=2015-01-01T01:00:50-04:00"
+	data, _ := httputils.GetDataFromUrl(url)
+	
+	var eventSummary sieve.EventSummary
+	json.Unmarshal(data, &eventSummary)
+	c.Assert(eventSummary.MinuteToDate, check.Equals, 5)
+}
+
+func (s *SieveEventsTestSuite) TestSieveCalculatesHourToDateCorrectly(c *check.C) {
+	var url string
+	
+	url = "http://localhost:9966/events/test"
+	httputils.PostDataToUrl(url, "application/json", []byte(`{"timestamp":"2015-01-01T01:04:00-04:00"}`))
+	httputils.PostDataToUrl(url, "application/json", []byte(`{"timestamp":"2015-01-01T01:15:00-04:00"}`))
+	httputils.PostDataToUrl(url, "application/json", []byte(`{"timestamp":"2015-01-01T01:22:00-04:00"}`))
+	httputils.PostDataToUrl(url, "application/json", []byte(`{"timestamp":"2015-01-01T01:38:00-04:00"}`))
+	httputils.PostDataToUrl(url, "application/json", []byte(`{"timestamp":"2015-01-01T01:42:00-04:00"}`))
+	
+	url = "http://localhost:9966/events/test?timestamp=2015-01-01T01:50:00-04:00"
+	data, _ := httputils.GetDataFromUrl(url)
+	
+	var eventSummary sieve.EventSummary
+	json.Unmarshal(data, &eventSummary)
+	c.Assert(eventSummary.HourToDate, check.Equals, 5)
+}
+
+func (s *SieveEventsTestSuite) TestSieveCalculatesProjectedThisMinuteCorrectly(c *check.C) {
+	var url string
+	
+	timestampString := "2015-01-01T01:00:29-04:00"
+	url = "http://localhost:9966/events/test"
+    jsonData := []byte(stringutils.Concat(`{"timestamp":"`, timestampString, `"}`))
+	
+	httputils.PostDataToUrl(url, "application/json", jsonData)
+	httputils.PostDataToUrl(url, "application/json", jsonData)
+	httputils.PostDataToUrl(url, "application/json", jsonData)
+	
+	url = stringutils.Concat("http://localhost:9966/events/test?timestamp=", timestampString)
+	data, _ := httputils.GetDataFromUrl(url)
+	
+	var eventSummary sieve.EventSummary
+	json.Unmarshal(data, &eventSummary)
+	c.Assert(eventSummary.ProjectedThisMinute, check.Equals, float32(6))
+}
+
+func (s *SieveEventsTestSuite) TestSieveCalculatesProjectedThisHourCorrectly(c *check.C) {
+	var url string
+	
+	timestampString := "2015-01-01T01:29:00-04:00"
+	url = "http://localhost:9966/events/test"
+    jsonData := []byte(stringutils.Concat(`{"timestamp":"`, timestampString, `"}`))
+	
+	httputils.PostDataToUrl(url, "application/json", jsonData)
+	httputils.PostDataToUrl(url, "application/json", jsonData)
+	httputils.PostDataToUrl(url, "application/json", jsonData)
+	
+	url = stringutils.Concat("http://localhost:9966/events/test?timestamp=", timestampString)
+	data, _ := httputils.GetDataFromUrl(url)
+	
+	var eventSummary sieve.EventSummary
+	json.Unmarshal(data, &eventSummary)
+	c.Assert(eventSummary.ProjectedThisHour, check.Equals, float32(6))
+}
+
